1d4ae187551463af7a0d98e05015dfae660b513d
Андрей Боев <a.boev.it@gmail.com>
2022-11-22 18:26:45 +0300

    Массивы

---
+++ Списки/input_array.py
@@ -0,0 +1,7 @@
+n = int(input('Введите размер массива: '))
+arr = []
+for i in range(n):
+    # append() - добавление элемента в конец массива
+    arr.append(int(input()))
+
+print(arr)
---
+++ Списки/input_array_from_line.py
@@ -0,0 +1,6 @@
+# 45 88 95 12 47 32
+# split() - разделяет строку на элементы по пробелам
+# map(func, array) - применяет функцию func ко всем элементам массива array
+# list преобразует в список
+nums = list(map(int, input().split()))
+print(nums)
